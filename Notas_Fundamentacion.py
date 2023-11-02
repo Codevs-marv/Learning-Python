0227428e54e6dd021fad55dc2452a1ca1cc9ca71
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-01 22:18:31 -0500

    Py-Notes-Fundaments

---
+++ Notas_Fundamentacion.py
@@ -0,0 +1,10 @@
+print("esto es un\nsalto de linea")  # \n hace un salto de linea
+print(r"C:\nombre\archivo")          # r al principio del string omite las funciones especiales 
+
+# P Y T H O N
+# 0 1 2 3 4 5
+palabra = "Python"  
+palabra[0]                           # letra P (índices del string)
+palabra[0:2]   # --> "Py"            # se omite el índice final 
+palabra[2:-1]  # --> "tho"
+palabra[2:]   # --> #thon            # con el segundo valor vacío muestra el último índice
