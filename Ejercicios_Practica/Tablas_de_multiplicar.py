40909722ead86d79888dd6c4f047763de80fa2bc
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-05 18:11:34 -0500

    Tablas_de_multiplicar

--- Ejercicios_Practica/Tablas_de_multiplicar.py
+++ Ejercicios_Practica/Tablas_de_multiplicar.py
@@ -1,3 +1,9 @@
 # E J E R C I C I O
 # Haz un programa que proporcione las tablas de multiplicar del 1 al 9
 
+
+for i in range(1,10):
+    print(f'Tabla del {i}')
+    for j in range(1,11):
+        print(f'{i} * {j} = {i*j}')
+    print()
