df57e62140ffcfdb594a846bf1b1975d82cb637c
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-06 13:22:02 -0500

    Adivinanza

---
+++ Ejercicios_Practica/Adivinanza.py
@@ -0,0 +1,4 @@
+# E J E R C I C I O
+''' Diseña un juego en el que el programa elija un número aleatorio y le pida al usuario que adivine ese número. Proporciona pistas (mayor/menor) y lleva un registro 
+de cuántos intentos se necesitaron para adivinar el número.'''
+
