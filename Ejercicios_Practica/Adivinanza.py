88551dd43ea79b0bdcdbace08869c9e6c2db9186
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-06 13:48:37 -0500

    Adivinanza_Numero

--- Ejercicios_Practica/Adivinanza.py
+++ Ejercicios_Practica/Adivinanza.py
@@ -2,3 +2,29 @@
 ''' Diseña un juego en el que el programa elija un número aleatorio y le pida al usuario que adivine ese número. Proporciona pistas (mayor/menor) y lleva un registro 
 de cuántos intentos se necesitaron para adivinar el número.'''
 
+import random
+
+print('Este es un juego de adivinanza! Tienes 4 intentos para adivinar el número.')
+print('¡BUENA SUERTE!')
+
+# numero que se tiene que adivinar
+numero_aleatorio = random.randint(1,10)
+
+# contador de intentos 
+intentos = 0
+
+while intentos <= 4:
+    if intentos == 0:
+        print('Es un número entero entre 1 y 10 (incluidos). ¡Intenta adivinarlo!')
+    else: 
+        print('Intentalo de nuevo!')
+
+    # numero elegido por el usuario
+    num_usuario = int(input('Ingresa tu opción: '))
+
+    if num_usuario == numero_aleatorio:
+        print('¡ADIVINASTE! :)')
+        break
+    else:
+        print('Erraste! :(')
+    intentos += 1
