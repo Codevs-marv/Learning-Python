7a4c341f0c5e68abef9ef66b891d29602d099b59
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-06 19:16:00 -0500

    Conversor

---
+++ Ejercicios_Practica/Conversor_Unidades.py
@@ -0,0 +1,3 @@
+# E J E R C I C I O
+#  Crea un programa que convierta entre diferentes unidades, como millas a kilómetros, grados Celsius a Fahrenheit, o cualquier otra conversión que elijas. Permite al usuario ingresar el valor y la unidad original,
+#  y muestra el resultado en la unidad deseada.
