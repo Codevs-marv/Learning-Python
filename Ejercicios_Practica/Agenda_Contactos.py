62b79a1485aebc5f56e9cfd023be608a8eed077f
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-06 19:07:50 -0500

    Agenda_Contactos

--- Ejercicios_Practica/Agenda_Contactos.py
+++ Ejercicios_Practica/Agenda_Contactos.py
@@ -3,106 +3,139 @@
 
 agenda = []
 
-print('¿Qué quieres hacer en tu agenda de contactos?')
-print("""
-    [1] Agregar contacto
-    [2] Editar contacto
-    [3] Buscar contacto
-    [4] Eliminar contacto
-""")
+while True:  # Bucle principal
+    print('¿Qué quieres hacer en tu agenda de contactos?')
+    print("""
+        [1] Agregar contacto
+        [2] Editar contacto
+        [3] Buscar contacto
+        [4] Eliminar contacto
+        [5] Salir
+    """)
 
-accion = int(input('Digite el número de la opción: '))
-cantidad = 0
+    accion = int(input('Digite el número de la opción: '))
 
-match accion:
-    # Agregar contacto
-    case 1:
-        contact = {}
-        contact[f'Amigo{cantidad}'] = {'Nombre':input('Ingrese nombre: '),'Celular':input('Ingrese N° celular: '),'Categoría':input('Indique tipo de relación con el contacto (amigo,primo,mamá,hermano,etc): ')}
-        agenda.append(contact)
-        cantidad += 1
+    if accion == 5:  # Opción para salir
+        break
+
+
+    match accion:
+        # Agregar contacto
+
+        case 1:
+            contact = {
+                'Nombre': input('Ingrese nombre: '),
+                'Celular': input('Ingrese N° celular: '),
+                'Categoria': input('Indique tipo de relación con el contacto (amigo,primo,mamá,hermano,etc): ')
+            }
+            agenda.append(contact)
+            print('Contacto agregado con éxito')
+
     
-    # Editar contacto
-    case 2:
-        while True:
-            editar = input('Ingrese el nombre del contacto que quiere editar: ')
-            encontrado = False
-            for contacto in agenda:
-                if contacto["Nombre"] == editar:
-                    encontrado = True
+        # Editar contacto
+        case 2:
+            while True:
+                editar = input('Ingrese el nombre del contacto que quiere editar: ')
+                encontrado = False
+                for contacto in agenda:
+                    if contacto["Nombre"] == editar:
+                        encontrado = True
+                        break
+                if not encontrado:
+                    print('Ese contacto no existe')
+                else:
                     break
-            if not encontrado:
-                print('Ese contacto no existe')
-            else:
-                break
 
-        print('¿Qué quiere editar?')
-        print("""
-            [1] Nombre
-            [2] Celular
-            [3] Categoría
-        """)
-        opcion = int(input("Digite el numero de la opcion: "))
+            print('¿Qué quiere editar?')
+            print("""
+                [1] Nombre
+                [2] Celular
+                [3] Categoría
+            """)
+            opcion = int(input("Digite el numero de la opcion: "))
 
-        # Cambiar nombre
-        if opcion == 1:
-            nuevo_nombre = input('Ingrese el nuevo nombre: ')
+            # Cambiar nombre
+            if opcion == 1:
+                nuevo_nombre = input('Ingrese el nuevo nombre: ')
 
-            for contacto in agenda:
-                if contacto["Nombre"] == editar:
+                for contacto in agenda:
                     contacto["Nombre"] = nuevo_nombre
                     print('El nombre del contacto ha sido actualizado')
                     break
 
-        # Cambiar celular            
-        elif opcion == 2:
-            nuevo_numero = int(input('Ingrese el nuevo número de celular: '))
+            # Cambiar celular            
+            elif opcion == 2:
+                nuevo_numero = int(input('Ingrese el nuevo número de celular: '))
 
-            for contacto in agenda:
-                if contacto['Celular'] == editar:
+                for contacto in agenda:
                     contacto['Celular'] = nuevo_numero
                     print('Número de celular actualizado')
                     break
 
-        # Cambiar categoria            
-        elif opcion == 3:
-            nueva_categoria = input('Ingrese la nueva categoría del contacto: ')
+            # Cambiar categoría            
+            elif opcion == 3:
+                nueva_categoria = input('Ingrese la nueva categoría del contacto: ')
 
-            for contacto in agenda:
-                if contacto['Categoría'] == editar:
-                    contacto['Categoría'] = nueva_categoria
+                for contacto in agenda:
+                    contacto['Categoria'] = nueva_categoria
                     print('Nueva categoría actualizada')
                     break
 
-    # Buscar contacto                
-    case 3:
-        print('¿Cómo quiere buscar el contacto?')
-        print("""
-            [1] Por nombre
-            [2] Por celular
-        """)
-        option = int(input('Digite el numero de la opción: '))
-        
-        # Buscar por nombre
-        if option == 1:
-            buscarnombre = input('Ingrese el nombre del contacto que desea buscar: ')
 
-            for contacto in agenda:
-                if contacto['Nombre'] == buscarnombre:
-                    print(contacto)
-                    break
-                else:
+        # Buscar contacto                
+        case 3:
+            print('¿Cómo quiere buscar el contacto?')
+            print("""
+                [1] Por nombre
+                [2] Por celular
+            """)
+            option = int(input('Digite el numero de la opción: '))
+            
+            # Buscar por nombre
+            if option == 1:
+                buscarnombre = input('Ingrese el nombre del contacto que desea buscar: ')
+
+                encontrado = False
+                for contacto in agenda:
+                    if contacto['Nombre'] == buscarnombre:
+                        print(contacto)
+                        encontrado = True
+                        break
+
+                if not encontrado:
                     print('El nombre no se encontró, el contacto no existe')
-                    break
 
-        # Buscar por N° celular        
-        else:
-            buscarcelular = int(input('Ingres el numero de celular que desea buscar'))
+            # Buscar por N° celular        
+            else:
+                buscarcelular = int(input('Ingrese el número de celular que desea buscar'))
+
+                encontrado = False
+                for contacto in agenda:
+                    if contacto['Celular'] == buscarcelular:
+                        print(contacto)
+                        encontrado = True
+                        break
+
+                if not encontrado:
+                    print('El número no está registrado, el contacto no existe')
+ 
+
+        # Eliminar contacto
+        case 4:
+            eliminar = input('Digite el nombre del contacto que desea eliminar: ')
 
             for contacto in agenda:
-                if contacto['Celular'] == buscarcelular:
-                    print(contacto)
-                    break
+                if contacto['Nombre'] == eliminar:
+                    print('¿Estás seguro de eliminar el contacto?')
+                    respuesta = input('Escriba "si" o "no": ')
+
+                    if respuesta == "si":
+                        agenda.remove(contacto)
+                        print('>Contacto eliminado<')
+                        break
+                    else:
+                        print('OK')
+                        break
                 else:
-                    print('El número no está registrado, el contacto no existe')
+                    print('El contacto no está registrado')
                     break
