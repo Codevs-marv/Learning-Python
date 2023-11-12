c90b6d7eb149b947b89411d7e4c93126be4ab5c7
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-12 16:42:46 -0500

    Simulador_Banco

---
+++ Ejercicios_Practica/Simulador_Banco.py
@@ -0,0 +1,4 @@
+# E J E R C I C I O
+'''Diseña un simulador de banco que permita a los usuarios crear cuentas, realizar depósitos y retiros, y consultar saldos. Implementa la lógica de manejo de cuentas y utiliza conceptos de manejo 
+de archivos o bases de datos para almacenar la información de las cuentas.'''
+
