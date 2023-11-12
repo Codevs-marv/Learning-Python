4b5173bdf5f625c400033fd638ed74e834aabac2
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-12 16:44:45 -0500

    Generador_Contrasenas

---
+++ Ejercicios_Practica/Generador_Contrasenas_Seguras.py
@@ -0,0 +1,3 @@
+# E J E R C I C I O
+'''Crea un programa que genere contraseñas seguras basadas en criterios específicos (longitud, caracteres especiales, letras mayúsculas y minúsculas, números, etc.). 
+Puedes permitir que los usuarios personalicen los criterios o generar automáticamente contraseñas seguras.'''
