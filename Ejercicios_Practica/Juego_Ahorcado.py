e220f906c728da1355a8371a7e69711d55e71fa4
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-12 15:00:06 -0500

    Ahorcado

--- Ejercicios_Practica/Juego_Ahorcado.py
+++ Ejercicios_Practica/Juego_Ahorcado.py
@@ -2,6 +2,16 @@
 '''Implementa un juego de ahorcado donde el jugador tiene que adivinar una palabra oculta. Puedes generar palabras aleatorias o permitir que el usuario ingrese palabras. 
 Añade funcionalidades como límites de intentos y la posibilidad de elegir categorías de palabras.'''
 
+import random
+
+nivel_1 = ['amor','paz','gato','perro','mundo','hola','lapiz','cielo','nube','agua','arbol','arroz','rojo','verde','azul','blanco','reloj','caja']
+nivel_2 = ['python','programa','aleatorio','codigo','variable','busqueda','computo,','edicion','operador','complejo','teclado','pantalla','elegante','elemento','secuencia']
+nievl_3 = ['comentario','ingeniero','explorador','incendio''conexion','estudiante','implementacion','arquitecto','temporada','desarrollo','evaluacion','establecimiento','destornillador','herramientas']
+nivel_4 = ['representante','espectacular','acondicionado','enciclopedia','extraordinario','desarrollador','contradictorio','administrador','inconveniente','anticonstitucional',
+           'especificacion','diferenciacion','internacional','sobresaliente']
+nivel_5 = ['quimericamente','anacronistico','inextricable','desacralizacion','obfuscacion','diseminacion','heterodoxamente','incorruptibilidad','indefatigable','inconmesurable',
+           'desproporcionado','inefablemente','irrefutablemente','desenmarañamiento','intransigencia']
+
 print('J U E G O   D E L   A H O R C A D O')
 print("""INSTRUCCIONES:
     1. Escoge un nivel de dificultad
@@ -31,4 +41,7 @@ while True:
             print('Nivel de dificultad no admitido, escoge de nuevo!')
         else:
             pass
+    
+    match dificultad:
         
+        case 1:
