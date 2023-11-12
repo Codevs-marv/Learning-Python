309647e449161311d1d51decfcecaa51e1e33259
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-12 13:31:11 -0500

    Juego_Ahorcado

---
+++ Ejercicios_Practica/Juego_Ahorcado.py
@@ -0,0 +1,4 @@
+# E J E R C I C I O
+'''Implementa un juego de ahorcado donde el jugador tiene que adivinar una palabra oculta. Puedes generar palabras aleatorias o permitir que el usuario ingrese palabras. 
+Añade funcionalidades como límites de intentos y la posibilidad de elegir categorías de palabras.'''
+
