90c72114032f943c467a7dd2c875e719c834c3e4
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-18 22:16:33 -0500

    Conversor_Unidades

--- Proyectos_practica/Fases 1 y 2/Conversor_Unidades.py
+++ Proyectos_practica/Fases 1 y 2/Conversor_Unidades.py
@@ -51,23 +51,139 @@ match opcion:
             # convertir a milimetros
             case 1:
                 result = 0
-                if und_inicial == 1:
-                    result = valor
-                    print(result)
-                elif (und_inicial == 2):
-                    result = valor * 10
-                    print(result)
-                elif (und_inicial == 3):
-                    result = valor * 25.4
-                    print(result)
-                elif (und_inicial == 4):
-                    result = valor * 304.8
-                    print(result)
-                elif (und_inicial == 5):
-                    result = valor * 1000
-                    print(result)
-                elif (und_inicial == 6):
-                    result = valor * 1000000
+
+                if 1 <= und_inicial <= 7 and 1 <= convertir_a <= 7: # comprobar opción
+                    if und_inicial == 1:
+                        result = valor
+                        print(result)
+                    elif (und_inicial == 2):
+                        result = valor * 10
+                        print(result)
+                    elif (und_inicial == 3):
+                        result = valor * 25.4
+                        print(result)
+                    elif (und_inicial == 4):
+                        result = valor * 304.8
+                        print(result)
+                    elif (und_inicial == 5):
+                        result = valor * 1000
+                        print(result)
+                    elif (und_inicial == 6):
+                        result = valor * 1000000
+                    else:
+                        result = valor * 1609344
+                        print(result)
                 else:
-                    result = valor * 1609344
-                    print(result)
+                    print('La opción ingresada no es válida')
+            
+            # convertir a centimetros
+            case 2:
+                result = 0
+
+                if 1 <= und_inicial <= 7 and 1 <= convertir_a <= 7: # comprobar opción
+                    if und_inicial == 2:
+                        result = valor
+                        print(result)
+                    elif (und_inicial == 1):
+                        result = valor / 10
+                        print(result)
+                    elif (und_inicial == 3):
+                        result = valor * 25.4
+                        print(result)
+                    elif (und_inicial == 4):
+                        result = valor * 30.48
+                        print(result)
+                    elif (und_inicial == 5):
+                        result = valor * 100
+                        print(result)
+                    elif (und_inicial == 6):
+                        result = valor * 100000
+                    else:
+                        result = valor * 160934.4
+                        print(result)
+                else:
+                    print('La opción ingresada no es válida')
+            
+            # convertir a pulgadas
+            case 3:
+                result = 0
+
+                if 1 <= und_inicial <= 7 and 1 <= convertir_a <= 7: # comprobar opción
+                    if und_inicial == 3:
+                        result = valor
+                        print(result)
+                    elif (und_inicial == 1):
+                        result = valor / 25.4
+                        print(result)
+                    elif (und_inicial == 2):
+                        result = valor * 2.54
+                        print(result)
+                    elif (und_inicial == 4):
+                        result = valor * 12
+                        print(result)
+                    elif (und_inicial == 5):
+                        result = valor * 39.37
+                        print(result)
+                    elif (und_inicial == 6):
+                        result = valor * 1000 * 39.37
+                    else:
+                        result = valor * 1609.344 * 39.37
+                        print(result)
+                else:
+                    print('La opción ingresada no es válida')
+            
+            # convertir a pies
+            case 4:
+                result = 0
+
+                if 1 <= und_inicial <= 7 and 1 <= convertir_a <= 7: # comprobar opción
+                    if und_inicial == 4:
+                        result = valor
+                        print(result)
+                    elif (und_inicial == 1):
+                        result = (valor / 1000) * 0.3048
+                        print(result)
+                    elif (und_inicial == 2):
+                        result = valor / 30.48
+                        print(result)
+                    elif (und_inicial == 3):
+                        result = valor / 12
+                        print(result)
+                    elif (und_inicial == 5):
+                        result = valor * 0.3048
+                        print(result)
+                    elif (und_inicial == 6):
+                        result = valor * 3280.84
+                    else:
+                        result = valor * 5280
+                        print(result)
+                else:
+                    print('La opción ingresada no es válida')
+
+            # convertir a metros
+            case 5:
+                result = 0
+
+                if 1 <= und_inicial <= 7 and 1 <= convertir_a <= 7: # comprobar opción
+                    if und_inicial == 5:
+                        result = valor
+                        print(result)
+                    elif (und_inicial == 1):
+                        result = (valor / 1000) * 0.3048
+                        print(result)
+                    elif (und_inicial == 2):
+                        result = valor / 30.48
+                        print(result)
+                    elif (und_inicial == 3):
+                        result = valor / 12
+                        print(result)
+                    elif (und_inicial == 4):
+                        result = valor * 0.3048
+                        print(result)
+                    elif (und_inicial == 6):
+                        result = valor * 3280.84
+                    else:
+                        result = valor * 5280
+                        print(result)
+                else:
+                    print('La opción ingresada no es válida')
