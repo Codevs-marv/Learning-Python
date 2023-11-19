2a3ce1cfb4d194603b7d24a9cdaac2cd77def097
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-18 20:02:52 -0500

    Herencia

---
+++ Fase 3 - Programacion Orientada a Objetos/Herencia.py
@@ -0,0 +1,10 @@
+# ¿QUÉ ES LA HERENCIA?
+# --> La capacidad que tiene una clase de heredar métodos y atributos de otra, además de agregar nuevos o midificar los heredados
+
+
+# CONTENIDO:
+# -> Implementar la herencia
+# -> Atributos y métodos heredados
+# -> Qué es el polimorfismo
+# -> Herencia múltiple
+
