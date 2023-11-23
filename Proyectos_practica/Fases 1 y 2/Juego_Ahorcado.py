333bbb0141b202e90bfb2e3738244438426a41b0
MarcoRodriguez <marcoansrv19@gmail.com>
2023-11-22 20:06:20 -0500

    Juego_Ahorcado

--- Proyectos_practica/Fases 1 y 2/Juego_Ahorcado.py
+++ Proyectos_practica/Fases 1 y 2/Juego_Ahorcado.py
@@ -4,12 +4,12 @@ Añade funcionalidades como límites de intentos y la posibilidad de elegir cate
 
 import random
 
-nivel_1 = ['amor', 'paz', 'gato', 'perro', 'mundo', 'hola', 'lapiz', 'cielo', 'nube', 'agua', 'arbol', 'arroz', 'rojo', 'verde', 'azul', 'blanco', 'reloj', 'caja']
-nivel_2 = ['python', 'programa', 'aleatorio', 'codigo', 'variable', 'busqueda', 'computo', 'edicion', 'operador', 'complejo', 'teclado', 'pantalla', 'elegante', 'elemento', 'secuencia']
-nivel_3 = ['comentario', 'ingeniero', 'explorador', 'incendio', 'conexion', 'estudiante', 'implementacion', 'arquitecto', 'temporada', 'desarrollo', 'evaluacion', 'establecimiento', 'destornillador', 'herramientas']
-nivel_4 = ['representante', 'espectacular', 'acondicionado', 'enciclopedia', 'extraordinario', 'desarrollador', 'contradictorio', 'administrador', 'inconveniente', 'anticonstitucional',
+nivel_1 = ['amor', 'paz', 'gato', 'perro', 'mundo', 'hola', 'lapiz', 'cielo', 'nube', 'agua', 'arbol', 'arroz', 'rojo', 'verde', 'azul', 'blanco', 'reloj', 'caja',
+            'python', 'programa', 'aleatorio', 'codigo', 'variable', 'busqueda', 'computo', 'edicion', 'operador', 'complejo', 'teclado', 'pantalla', 'elegante', 'elemento', 'secuencia']
+nivel_2 = ['comentario', 'ingeniero', 'explorador', 'incendio', 'conexion', 'estudiante', 'implementacion', 'arquitecto', 'temporada', 'desarrollo', 'evaluacion', 'establecimiento', 'destornillador', 'herramientas'
+            'representante', 'espectacular', 'acondicionado', 'enciclopedia', 'extraordinario', 'desarrollador', 'contradictorio', 'administrador', 'inconveniente', 'anticonstitucional',
            'especificacion', 'diferenciacion', 'internacional', 'sobresaliente']
-nivel_5 = ['quimericamente', 'anacronistico', 'inextricable', 'desacralizacion', 'obfuscacion', 'diseminacion', 'heterodoxamente', 'incorruptibilidad', 'indefatigable', 'inconmesurable',
+nivel_3 = ['quimericamente', 'anacronistico', 'inextricable', 'desacralizacion', 'obfuscacion', 'diseminacion', 'heterodoxamente', 'incorruptibilidad', 'indefatigable', 'inconmesurable',
            'desproporcionado', 'inefablemente', 'irrefutablemente', 'desenmarañamiento', 'intransigencia']
 
 print('J U E G O   D E L   A H O R C A D O')
@@ -21,7 +21,11 @@ print("""INSTRUCCIONES:
 while True:
 
     print('\n¿Empezamos?  :)')
-    start = input('Ingresa "si" o "no"')
+    try:
+        start = input('Ingresa "si" o "no"\n')
+    except Exception as e:
+        print(type.__name__, 'Ingresa una opción válida')
+        continue
     start = start.lower()  # convertir la entrada a minúsculas
 
     if start == 'no':  # salir
@@ -30,20 +34,137 @@ while True:
 
     else:  # comienza el juego
         print("""Escoge la dificultad:
-        1. Muy fácil
-        2. Fácil
-        3. Media
-        4. Difícil
-        5. Avanzado""")
-        dificultad = int(input('Ingresa el numero indicativo de dificultad: '))
+        1. Fácil
+        2. Media
+        3. Difícil
+        """)
+        try:
+            dificultad = int(input('Ingresa el numero indicativo de dificultad: '))
+        except ValueError:
+            print('Ingrese una opción válida')
+            continue
 
-        if not (1 <= dificultad <= 5):
+        if not (1 <= dificultad <= 3):
             print('Nivel de dificultad no admitido, escoge de nuevo!')
-        else:
-            pass
+            continue
 
     completado = False  # inicializar la variable completado
 
     match dificultad:
+
+        # Nivel Fácil
         case 1:
-            pass
+            Logrado = False
+            palabra = random.choice(nivel_1)  # palabra al azar
+            cont = len(palabra) + 3
+
+            print('Adivina la palabra!\n')
+            oculta = ['_'] * len(palabra)
+            adiv = list(palabra)
+            print(" ".join(oculta))
+
+            while cont > 0 and not Logrado:
+                try:
+                    letra = input('Ingresa una letra (la que creas que va): ')
+                except ValueError:
+                    print('Debes ingresar una letra')
+                    continue
+
+                acertaste = False
+                for i in range(len(adiv)):
+                    if adiv[i] == letra:
+                        oculta[i] = letra
+                        acertaste = True
+
+                if acertaste:
+                    print(" ".join(oculta))
+                    print()
+                    
+                    if "".join(oculta) == palabra:
+                        Logrado = True
+                        print('¡Felicidades! Has adivinado la palabra:', palabra)
+                else:
+                    print('No acertaste, pero aún tienes más intentos! ;)\n')
+
+                cont -= 1
+
+            if not Logrado and cont == 0:
+                print(f'Has agotado tus intentos.\nLa palabra era: {palabra}')
+                print('Buena suerte a la próxima ;-)')
+        
+        # Nivel Medio
+        case 2:
+            Logrado = False
+            palabra = random.choice(nivel_2)  # palabra al azar
+            cont = len(palabra) + 3
+
+            print('Adivina la palabra!\n')
+            oculta = ['_'] * len(palabra)
+            adiv = list(palabra)
+            print(" ".join(oculta))
+
+            while cont > 0 and not Logrado:
+                try:
+                    letra = input('Ingresa una letra (la que creas que va): ')
+                except ValueError:
+                    print('Debes ingresar una letra')
+                    continue
+
+                acertaste = False
+                for i in range(len(adiv)):
+                    if adiv[i] == letra:
+                        oculta[i] = letra
+                        acertaste = True
+
+                if acertaste:
+                    print(" ".join(oculta))
+                    print()
+                    
+                    if "".join(oculta) == palabra:
+                        Logrado = True
+                        print('¡Felicidades! Has adivinado la palabra:', palabra)
+                else:
+                    print('No acertaste, pero aún tienes más intentos! ;)\n')
+
+                cont -= 1
+
+            if not Logrado and cont == 0:
+                print(f'Has agotado tus intentos.\nLa palabra era: {palabra}')
+
+        # Nivel Difícil
+        case 3:
+            Logrado = False
+            palabra = random.choice(nivel_3)  # palabra al azar
+            cont = len(palabra) + 3
+
+            print('Adivina la palabra!\n')
+            oculta = ['_'] * len(palabra)
+            adiv = list(palabra)
+            print(" ".join(oculta))
+
+            while cont > 0 and not Logrado:
+                try:
+                    letra = input('Ingresa una letra (la que creas que va): ')
+                except ValueError:
+                    print('Debes ingresar una letra')
+
+                acertaste = False
+                for i in range(len(adiv)):
+                    if adiv[i] == letra:
+                        oculta[i] = letra
+                        acertaste = True
+
+                if acertaste:
+                    print(" ".join(oculta))
+                    print()
+                    
+                    if "".join(oculta) == palabra:
+                        Logrado = True
+                        print('¡Felicidades! Has adivinado la palabra:', palabra)
+                else:
+                    print('No acertaste, pero aún tienes más intentos! ;)\n')
+
+                cont -= 1
+
+            if not Logrado and cont == 0:
+                print(f'Has agotado tus intentos.\nLa palabra era: {palabra}')
