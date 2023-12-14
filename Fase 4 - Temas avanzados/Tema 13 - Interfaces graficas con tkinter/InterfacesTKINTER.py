c4bf94aa1ed1d55c2e2d9b0b3e9dc785c5c8d27b
MarcoRodriguez <marcoansrv19@gmail.com>
2023-12-13 20:06:50 -0500

    Interfaces

--- Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/InterfacesTKINTER.py
+++ Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/InterfacesTKINTER.py
@@ -37,7 +37,7 @@ root.mainloop()
 
 
 
-#>> FRAMES
+#>> F R A M E S
 
 # Frame(root): Crear el frame dentro de la raiz root
 # .pack(): Colocar el widget en la ventana
@@ -72,7 +72,7 @@ root.mainloop()
 
 
 
-#>> LABELS
+#>> L A B E L S
 
 # Configuracion raiz
 """root = Tk()
@@ -99,7 +99,7 @@ root.mainloop()"""
 
 
 
-#>> ENTRY
+#>> E N T R Y
 # Es una entrada de texto
 
 
@@ -109,17 +109,101 @@ root = Tk()
 # Entrada
 entry = Entry(root)
 entry.grid(row=0,column=1)
+entry.config(justify='right') # entrada de text en el entry a la derecha
 
 # Texto
 label = Label(root, text='Nombre')
 label.grid(row=0,column=0,sticky='e')
 
 
-
 entry2 = Entry(root)
 entry2.grid(row=1,column=1)
+entry2.config(justify='center') # entrada de text en el entry en el centro
+entry2.config(show='*') # ocultar lo que estamos ingresando
 
-label2 = Label(root, text='Apellidos')
+label2 = Label(root, text='Contraseña')
 label2.grid(row=1,column=0,sticky='e')
+
+root.mainloop()
+
+
+
+
+
+
+
+#>> B U T T O N S
+
+def sumar():
+    r.set(float(n1.get()) + float(n2.get()))
+    borrar()
+
+def resta():
+    r.set(float(n1.get()) - float(n2.get()))
+    borrar()
+
+def producto():
+    r.set(float(n1.get()) * float(n2.get()))
+    borrar()
+
+def borrar():
+    n1.set('')
+    n2.set('')
+
+root = Tk()
+
+# variables
+n1 = StringVar()
+n2 = StringVar()
+r = StringVar()
+
+# Entradas
+Label(root, text='Num 1').pack()
+Entry(root, justify='center', textvariable=n1).pack() # 1er numero
+Label(root, text='Num 2').pack()
+Entry(root, justify='center', textvariable=n2).pack() # 2do numero
+
+# Boton SUMAR
+Button(root, text='Sumar', command=sumar).pack()
+
+# Boton resta
+Button(root, text='Restar', command=resta).pack()
+
+# Boton producto
+Button(root, text='Producto', command=producto).pack()
+
+
+# Salida
+Label(root, text='\nResult ->').pack()
+Entry(root, justify='center', textvariable=r, state='disabled').pack() # result
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+#>> R A D I O   B U T T O N S
+
+# Funcion para mostrar opcion seleccionada
+def seleccionar():
+    monitor.config(text='{}'.format(opcion.get()))
+
+
+
+root = Tk()
+
+opcion = IntVar()
+
+Radiobutton(root, text='Opcion 1', variable=opcion, value=1, command=seleccionar).pack()
+Radiobutton(root, text='Opcion 2', variable=opcion, value=2, command=seleccionar).pack()
+Radiobutton(root, text='Opcion 3', variable=opcion, value=3, command=seleccionar).pack()
+
+monitor = Label(root)
+monitor.pack()
 
 root.mainloop()
