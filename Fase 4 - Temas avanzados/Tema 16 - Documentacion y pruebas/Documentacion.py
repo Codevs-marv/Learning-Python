e47bd384005b2d62f0a40f98b11a900bad667c78
MarcoRodriguez <marcoansrv19@gmail.com>
2024-01-25 16:35:21 -0500

    Docstring

---
+++ Fase 4 - Temas avanzados/Tema 16 - Documentacion y pruebas/Documentacion.py
@@ -0,0 +1,5 @@
+'''La documentación es la parte del desarrollo que permite explicar el funcionamiento del codigo, tanto para nosotros mismo en el futuro,
+como para otras personas'''
+
+'''Las pruebas sirven para validar el funcionamiento y encontrar posibles fallos'''
+
