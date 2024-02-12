00d9c1d0f0cee9d8871d91f7a08b4c66ac0a0385
MarcoRodriguez <marcoansrv19@gmail.com>
2024-02-11 21:36:54 -0500

    update database

--- Gestor/tests/test_database.py
+++ Gestor/tests/test_database.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 import database as db
 
@@ -15,3 +16,22 @@ class TestDatabase(unittest.TestCase):
         cliente_inexistente = db.Clientes.buscar('123Z')
         self.assertIsNotNone(cliente_existente)
         self.assertIsNone(cliente_inexistente)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('19RV', 'Marco', 'Rodriguez')
+        self.assertEqual(len(db.Clientes.lista), 4)
+        self.assertEqual(nuevo_cliente.cc, '19RV')
+        self.assertEqual(nuevo_cliente.nombre, 'Marco')
+        self.assertEqual(nuevo_cliente.apellido, 'Rodriguez')
+
+    def test_modificar_cliente(self):
+        cliente_modificar = copy.copy(db.Clientes.buscar('12A'))
+        cliente_modificado = db.Clientes.modificar('12A', 'Mariana', 'Perez')
+        self.assertEqual(cliente_modificar.nombre, 'Martha')
+        self.assertEqual(cliente_modificado.nombre, 'Mariana')
+
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.borrar('13B')
+        cliente_rebuscado = db.Clientes.buscar('13B')
+        self.assertEqual(cliente_borrado.cc, '13B')
+        self.assertIsNone(cliente_rebuscado)
